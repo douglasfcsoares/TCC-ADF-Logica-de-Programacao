82e74e9cf12af3c25e78e9de9c9f9fcbb725463e
douglasfcsoares <douglasfcsoares@gmail.com>
2025-02-10 22:06:24 -0300

    TCC fase 2, adicionando endereço com armazenamento dos dados em variáveis

--- lib/first_script.dart
+++ lib/first_script.dart
@@ -1,8 +1,16 @@
 void main() {
   boasVindas();
+  endereco();
 }
 
 void boasVindas() {
   print(
       'Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de auto atendimento.');
+}
+
+void endereco() {
+  var end = 'Av. Governador Ivo Silvera 1413';
+  var tel = '48 9 9139-5727';
+
+  print('A nossa loja fica na $end, e no telefone $tel');
 }
